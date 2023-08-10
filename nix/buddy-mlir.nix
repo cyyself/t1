1f27c92936e654890b8be151dc90bfad3b2223d1
Avimitin <dev@avimit.in>
2023-08-10 19:57:04 +0800

    [buddy] rewrite the derivation into one-step build phase
    Signed-off-by: Avimitin <dev@avimit.in>

--- nix/buddy-mlir.nix
+++ nix/buddy-mlir.nix
@@ -1,68 +1,57 @@
-{ stdenv
-, runCommand
-, lib
-, cmake
-, coreutils
-, python3
-, git
-, fetchFromGitHub
-, ninja
-}:
+{ stdenv, cmake, ninja, python3, fetchFromGitHub }:
 
-let
-  llvm = stdenv.mkDerivation rec {
-    pname = "llvm-project";
-    version = "unstable-2023-05-02";
-    requiredSystemFeatures = [ "big-parallel" ];
-    nativeBuildInputs = [ cmake ninja python3 ];
-    src = fetchFromGitHub {
-      owner = "llvm";
-      repo = pname;
-      rev = "8f966cedea594d9a91e585e88a80a42c04049e6c";
-      hash = "sha256-g2cYk3/iyUvmIG0QCQpYmWj4L2H4znx9KbuA5TvIjrc=";
-    };
-    cmakeDir = "../llvm";
-    cmakeFlags = [
-      "-DLLVM_ENABLE_BINDINGS=OFF"
-      "-DLLVM_ENABLE_OCAMLDOC=OFF"
-      "-DLLVM_BUILD_EXAMPLES=OFF"
-      "-DLLVM_ENABLE_PROJECTS=mlir;clang"
-      "-DLLVM_TARGETS_TO_BUILD=host;RISCV"
-      "-DLLVM_INSTALL_UTILS=ON"
-    ];
-    checkTarget = "check-mlir check-clang";
-    postInstall = ''
-      cp include/llvm/Config/config.h $out/include/llvm/Config
-    '';
-  };
-  mlir_dir = runCommand "mlir_dir" { } ''
-    mkdir -p $out
-    ln -s ${llvm.src}/* $out
-    cp -r ${llvm} $out/build
-  '';
-
-in
 stdenv.mkDerivation rec {
   pname = "buddy-mlir";
   version = "unstable-2023-05-26";
-  src = fetchFromGitHub {
-    owner = "buddy-compiler";
-    repo = pname;
-    rev = "74c18e6963cf4781be254d3c5d963b36c0642ba4";
-    hash = "sha256-Wx/QQrELfOT0h4B8hF9EPZKn4yVHBZeYh3Wm85Jpq60=";
-  };
+  srcs = [
+    (fetchFromGitHub {
+      owner = "buddy-compiler";
+      repo = "buddy-mlir";
+      rev = "05f9099740dc1f9dbea6bb828bc1cae1de618be5";
+      sha256 = "sha256-/xI4agRpdmAgWjRayk+qcG6F/L5/6alYJxtrS8NTKA0=";
+    })
 
-  ninjaFlags = [ "buddy-translate" ];
+    (fetchFromGitHub {
+      owner = "llvm";
+      repo = "llvm-project";
+      rev = "8f966cedea594d9a91e585e88a80a42c04049e6c";
+      sha256 = "sha256-g2cYk3/iyUvmIG0QCQpYmWj4L2H4znx9KbuA5TvIjrc=";
+    })
+  ];
+  sourceRoot = ".";
+  unpackPhase = ''
+    sources=($srcs)
+    # [0] buddy-mlir
+    cp -rT ''${sources[0]} buddy-mlir
+    # sources are copy from RO store
+    chmod -R u+w -- buddy-mlir
+    # [1] llvm: cmake is hard-coded to find llvm inside buddy-mlir
+    cp -rT ''${sources[1]} buddy-mlir/llvm
+    chmod -R u+w -- buddy-mlir/llvm
+  '';
+  # Bash variable is not resolved in cmakeFlags
+  preConfigure = ''
+    cmakeFlagsArray+=(
+      -DLLVM_EXTERNAL_BUDDY_MLIR_SOURCE_DIR="$(realpath buddy-mlir)"
+    )
+  '';
 
   requiredSystemFeatures = [ "big-parallel" ];
 
-  nativeBuildInputs = [ cmake ninja ];
+  nativeBuildInputs = [ cmake ninja python3 ];
 
-  passthru = { inherit llvm; };
-
+  # CMakeList is available in llvm main source inside the llvm repo
+  cmakeDir = "../buddy-mlir/llvm/llvm";
   cmakeFlags = [
-    "-DMLIR_DIR=${mlir_dir}/build/lib/cmake/mlir"
-    "-DLLVM_DIR=${mlir_dir}/build/lib/cmake/llvm"
+    "-DLLVM_ENABLE_BINDINGS=OFF"
+    "-DLLVM_ENABLE_OCAMLDOC=OFF"
+    "-DLLVM_BUILD_EXAMPLES=OFF"
+    "-DLLVM_ENABLE_PROJECTS=mlir;clang"
+    "-DLLVM_TARGETS_TO_BUILD=host;RISCV"
+    "-DLLVM_INSTALL_UTILS=ON"
+    "-DLLVM_ENABLE_ASSERTIONS=ON"
+    "-DLLVM_EXTERNAL_PROJECTS=${pname}"
   ];
-}
 
+  checkTarget = "check-mlir check-clang check-buddy";
+}
